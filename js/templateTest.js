4f0699bff0073e8a1168f256bf53488e6a1bef17
seungwon <seungwon9509@gmail.com>
2020-01-03 09:08:49 +0900

    base

---
+++ js/templateTest.js
@@ -0,0 +1,9 @@
+var items = new Vue({
+    el: "#test",
+    data: {
+        items: [
+            { title: 'test1', imgSrc: '' },
+            { title: 'test2', imgSrc: '' },
+        ]
+    }
+})
