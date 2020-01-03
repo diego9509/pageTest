937fbd71433cbf133c7219345f8992ae9890a596
seungwon <seungwon9509@gmail.com>
2020-01-03 11:06:06 +0900

    flex based basic layout

--- js/templateTest.js
+++ js/templateTest.js
@@ -2,8 +2,10 @@ var items = new Vue({
     el: "#test",
     data: {
         items: [
-            { title: 'test1', imgSrc: '' },
-            { title: 'test2', imgSrc: '' },
+            { title: 'test1', imgSrc: 'img/about/1.jpg' },
+            { title: 'test2', imgSrc: 'img/about/2.jpg' },
+            { title: 'test3', imgSrc: 'img/about/3.jpg' },
+            { title: 'test4', imgSrc: 'img/about/4.jpg' },
         ]
     }
 })
