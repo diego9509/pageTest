68493cea8aebea14f2fb098e027e01eab90c4f42
leesw <seungwon9509@gmail.com>
2020-01-12 17:48:16 +0900

    base layout fix

--- js/map.js
+++ js/map.js
@@ -1,6 +1,6 @@
 var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
 var options = { //지도를 생성할 때 필요한 기본 옵션
-	center: new kakao.maps.LatLng(37.4374733452905, 126.80155746307413), //지도의 중심좌표.
+	center: new kakao.maps.LatLng(37.437157915800555, 126.80151309800458), //지도의 중심좌표.
     level: 3,
     draggable : false,
     scrollwheel : false, //지도의 레벨(확대, 축소 정도)
@@ -9,7 +9,7 @@ var options = { //지도를 생성할 때 필요한 기본 옵션
 var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
 
 // 마커가 표시될 위치입니다 
-var markerPosition  = new kakao.maps.LatLng(37.4374733452905, 126.80155746307413); 
+var markerPosition  = new kakao.maps.LatLng(37.437157915800555, 126.80151309800458); 
 
 // 마커를 생성합니다
 var marker = new kakao.maps.Marker({
